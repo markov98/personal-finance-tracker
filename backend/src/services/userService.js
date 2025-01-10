7fa9cb778ff39c064c77edd0e8a9fb1908d45050
Martin Markov <marto98nm@gmail.com>
2025-01-10 19:01:49 +0200

    fixed userService to work with better-sqlite3

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,23 +1,24 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
-const db = require('../config/db');
+const db = require('../config/db')
 const { SECRET } = require("../constants");
 
 exports.register = async (username, email, password) => {
     const hashedPassword = await bcrypt.hash(password, 10);
-
-    const { lastID } = await db.run(`
+    const stmt = db.prepare(`
         INSERT INTO users (username, email, password)
         VALUES (?, ?, ?)
-    `, [username, email, hashedPassword]);
-
-    const user = { id: lastID, email };
+    `);
+    
+    const info = stmt.run(username, email, hashedPassword);
+    
+    const user = { id: info.lastInsertRowid, email };
     return getResult(user);
 };
 
 exports.login = async (email, password) => {
-    const user = await db.get('SELECT id, email, username, password FROM users WHERE email = ?', [email]);
+    const stmt = db.prepare('SELECT id, email, username, password FROM users WHERE email = ?');
+    const user = stmt.get(email);
 
     if (!user) {
         throw new Error('User not found');
