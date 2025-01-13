19fd99b7a2ae56f9e1c14be92ceaef8d221555e1
Martin Markov <marto98nm@gmail.com>
2025-01-13 19:48:23 +0200

    added removeBalance method

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -49,6 +49,34 @@ exports.addBalance = async (email, amount) => {
     return { message: 'Balance updated successfully' };
 };
 
+exports.removeBalance = async (username, amount) => {
+    // Fetching the user's current ballance
+    const getUserStmt = db.prepare('SELECT balance FROM users WHERE id = ?');
+    const user = getUserStmt.get(userId);
+
+    if (!user) {
+        throw new Error('User not found');
+    }
+
+    if (user.balance < amount) {
+        throw new Error('Insufficient balance');
+    }
+
+    // If enough balance, proceed to update it
+    const updateStmt = db.prepare(`
+        UPDATE users
+        SET balance = balance - ?
+        WHERE username = ?
+    `);
+    const info = updateStmt.run(amount, username);
+
+    if (info.changes === 0) {
+        throw new Error('Update failed');
+    }
+
+    return { message: 'Balance removed successfully' };
+};
+
 
 function getResult(user) {
     const payload = { _id: user.id, email: user.email };
