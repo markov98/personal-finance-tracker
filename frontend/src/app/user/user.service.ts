6589b76d41b62311e60c8142a04f0b8c7e6268dc
Martin Markov <marto98nm@gmail.com>
2025-02-02 10:36:14 +0200

    updated userservice

---
+++ frontend/src/app/user/user.service.ts
@@ -0,0 +1,61 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { UserForAuth } from '../types/user';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Subscription, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService implements OnDestroy {
+  private user$$ = new BehaviorSubject<UserForAuth | undefined>(undefined);
+  private user$ = this.user$$.asObservable();
+
+  user: UserForAuth | undefined;
+  USER_KEY = '[user]';
+
+  userSubscription: Subscription;
+
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor(private http: HttpClient) {
+    this.userSubscription = this.user$.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<UserForAuth>('/api/login', { email, password })
+      .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  register(
+    username: string,
+    email: string,
+    tel: string,
+    password: string,
+    rePassword: string
+  ) {
+    return this.http
+      .post<UserForAuth>('/api/register', {
+        username,
+        email,
+        tel,
+        password,
+        rePassword,
+      })
+      .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  logout() {
+    return this.http
+      .post('/api/logout', {})
+      .pipe(tap(() => this.user$$.next(undefined)));
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
+}
