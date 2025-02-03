0bd6e1c42ebba1651b6af5258dded3965eeb2769
Martin Markov <marto98nm@gmail.com>
2025-02-03 15:58:59 +0200

    updated login componenr

--- frontend/src/app/user/login/login.component.ts
+++ frontend/src/app/user/login/login.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  standalone: false,
 })
 export class LoginComponent {
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
 
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  login(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const { email, password } = this.form.value;
+
+    this.userService.login(email!, password!)
+      .subscribe(() => {
+        this.router.navigate(['/']);
+      });
+  }
 }
