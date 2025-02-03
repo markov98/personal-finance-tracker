497be0d62e545e104e3dba9912a6c82733ba948f
Martin Markov <marto98nm@gmail.com>
2025-02-03 09:30:02 +0200

    removing unneeded code

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { UserModule } from './user/user.module';
 
 @Component({
   selector: 'app-root',
