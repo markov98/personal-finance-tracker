ef6d8ec5f4445779c07d4c117bdb19529b64a217
Martin Markov <marto98nm@gmail.com>
2025-02-17 06:59:04 +0200

    created authenticate component

---
+++ frontend/src/app/authenticate/authenticate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authenticate',
+  imports: [],
+  templateUrl: './authenticate.component.html',
+  styleUrl: './authenticate.component.css'
+})
+export class AuthenticateComponent {
+
+}
