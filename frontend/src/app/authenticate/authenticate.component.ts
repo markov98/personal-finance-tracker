138176e77763341d784ee95fa1b2757a130be5c4
Martin Markov <marto98nm@gmail.com>
2025-02-17 07:35:28 +0200

    updated authenticate element

--- frontend/src/app/authenticate/authenticate.component.ts
+++ frontend/src/app/authenticate/authenticate.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-authenticate',
-  imports: [],
   templateUrl: './authenticate.component.html',
-  styleUrl: './authenticate.component.css'
+  styleUrls: ['./authenticate.component.css'],
 })
-export class AuthenticateComponent {
+export class AuthenticateComponent implements OnInit {
+  isAuthenticating = true;
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUser().subscribe({
+      next: () => {
+        this.isAuthenticating = false;
+      },
+      error: () => {
+        this.isAuthenticating = false;
+      },
+      complete: () => {
+        this.isAuthenticating = false;
+      },
+    });
+  }
 }
