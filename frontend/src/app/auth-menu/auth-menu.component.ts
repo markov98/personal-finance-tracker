0e829972bb3d4ba3cb90e9dc50ad5bd5e5c915b1
Martin Markov <marto98nm@gmail.com>
2025-03-04 17:19:29 +0200

    moved auth menu to saperate component

---
+++ frontend/src/app/auth-menu/auth-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-menu',
+  imports: [],
+  templateUrl: './auth-menu.component.html',
+  styleUrl: './auth-menu.component.css'
+})
+export class AuthMenuComponent {
+
+}
