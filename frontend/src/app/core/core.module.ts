8fe73bff4d6fb2eabeb3210f11499cfb821e9a27
Martin Markov <marto98nm@gmail.com>
2025-03-08 14:55:55 +0200

    moved header and footer component to  core module

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorComponent } from './error/error.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [ErrorComponent],
+  declarations: [ErrorComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule
   ]
