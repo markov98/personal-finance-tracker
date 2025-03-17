24208f6a9094262cc6d99decc22a520070375644
Martin Markov <marto98nm@gmail.com>
2025-03-17 03:45:31 +0200

    working on finace service

--- frontend/src/app/finance/finance.service.ts
+++ frontend/src/app/finance/finance.service.ts
@@ -8,7 +8,18 @@ export class FinanceService {
 
   constructor(private http: HttpClient) { }
 
-  addMoney() { }
+  addBalance(email: string, amount: number) {
+    return this.http
+      .post('api/balnce/add', { email, amount })
+  }
 
-  removeMoney() { }
+  removeBalance(email: string, amount: number) {
+    return this.http
+      .post('api/balnce/remove', { email, amount })
+  }
+
+  getBalance(email: string) {
+    return this.http
+      .get(`api/balnce/get?${email}`)
+  }
 }
