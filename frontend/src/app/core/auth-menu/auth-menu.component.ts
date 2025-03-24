665e9aa699e99d2f77b3366c14fe642d57004ff1
Martin Markov <marto98nm@gmail.com>
2025-03-24 18:30:45 +0200

    updated authmenu component made it dynamic

--- frontend/src/app/core/auth-menu/auth-menu.component.ts
+++ frontend/src/app/core/auth-menu/auth-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../user/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-menu',
@@ -7,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './auth-menu.component.css'
 })
 export class AuthMenuComponent {
+  constructor(private userService: UserService, private router: Router) { }
 
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+
+  get firstName(): string {
+    return this.userService.user?.username || '';
+  }
+
+  logout() {
+    this.userService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+      error: () => {
+        this.router.navigate(['/login']);
+      },
+    });
+  }
 }
