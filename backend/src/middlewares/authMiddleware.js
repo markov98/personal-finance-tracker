26f3cffa1f9630abf8d85e10e0b8c4987ec2d072
Martin Markov <marto98nm@gmail.com>
2025-03-29 03:19:04 +0200

    adding comments to the backend

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,8 +1,9 @@
-const jwt = require("jsonwebtoken");
-const { SECRET } = require("../constants");
+const jwt = require("jsonwebtoken"); 
+const { SECRET } = require("../constants"); 
 
 const revokedTokens = [];
 
+// Middleware for token authentication
 exports.auth = (req, res, next) => {
   req.token = req.header("X-Authorization");
 
@@ -10,24 +11,25 @@ exports.auth = (req, res, next) => {
     try {
       const decodedToken = jwt.verify(req.token, SECRET);
       req.user = decodedToken;
-
       next();
     } catch (error) {
       res.status(401).json({ message: "You are not authorized!" });
     }
   } else {
-    next();
+    next(); 
   }
 };
 
+// Middleware to check user authentication
 exports.isAuth = (req, res, next) => {
   if (!req.user) {
     res.status(401).json({ message: "You are not authorized!" });
   } else {
-      next()
+    next();
   }
 };
 
+// Function to revoke tokens
 exports.revokeToken = (token) => {
   revokedTokens.push(token);
 };
