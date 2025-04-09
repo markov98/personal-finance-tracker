11aac734b9c5a2707a63d53ceb67fa48204ad8c7
Martin Markov <marto98nm@gmail.com>
2025-04-09 08:26:00 +0300

    added error to routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './core/error/error.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'error', component: ErrorComponent }
 ];
 
 @NgModule({
