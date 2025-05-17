445cbbd127e956ceac88a8b8fe3e02453963b0c9
Martin Markov <marto98nm@gmail.com>
2025-05-17 10:40:11 +0300

    added get-transaction route to backend balanceController

--- backend/src/controllers/balanceController.js
+++ backend/src/controllers/balanceController.js
@@ -13,7 +13,18 @@ router.post('/transaction', isAuth, async (req, res) => {
     }
 });
 
-router.get('/get', isAuth, async (req, res) => {
+router.get('/get-transactions', isAuth, async (req, res) => {
+    try {
+        const { userId } = req.query;
+        const result = await balanceService.getTransactions(userId);
+        res.json(result);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.get('/get-balance', isAuth, async (req, res) => {
     try {
         const { userId } = req.query;
         const result = await balanceService.getBalance(userId);
