e4c916198e4de62cd2de69592de49f5d96459810
Martin Markov <marto98nm@gmail.com>
2025-06-18 14:11:29 +0300

    worked on transaction list component

--- frontend/src/app/finance/transaction-list/transaction-list.component.ts
+++ frontend/src/app/finance/transaction-list/transaction-list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FinanceService } from '../finance.service';
+import { Transaction } from '../../types/transactions';
+import { UserService } from '../../user/user.service';
 
 @Component({
   selector: 'app-transaction-list',
   standalone: false,
   templateUrl: './transaction-list.component.html',
-  styleUrl: './transaction-list.component.css',
+  styleUrls: ['./transaction-list.component.css'],
 })
-export class TransactionListComponent {
+export class TransactionListComponent implements OnInit {
+  transactions: Transaction[] = [];
 
+  constructor(private financeService: FinanceService, private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.financeService.getTransactions(this.userService.userId).subscribe({
+      next: (data) => {
+        this.transactions = data;
+      },
+      error: (err) => {
+        console.error('Failed to load transactions:', err);
+      }
+    });
+  }
 }
