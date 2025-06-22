dec033d3ea449565d14dd6b6af15d21cb4d97192
Martin Markov <marto98nm@gmail.com>
2025-06-22 17:30:58 +0300

    added authguards

--- frontend/src/app/finance/finance-routing.module.ts
+++ frontend/src/app/finance/finance-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddTransactionComponent } from './add-transaction/add-transaction.component';
 import { TransactionListComponent } from './transaction-list/transaction-list.component';
+import { AuthGuard } from '../guards/auth-guard.guard';
 
 export const routes: Routes = [
-    { path: 'add-transaction', component: AddTransactionComponent },
-    { path: 'transaction-list', component: TransactionListComponent }
+    { path: 'add-transaction', component: AddTransactionComponent, canActivate: [AuthGuard] },
+    { path: 'transaction-list', component: TransactionListComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
--- frontend/src/app/user/user-routing.module.ts
+++ frontend/src/app/user/user-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { LoginRedirect } from '../guards/redirect-login.guard';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent },
+    { path: 'login', component: LoginComponent, canActivate: [LoginRedirect] },
+    { path: 'signup', component: SignupComponent, canActivate: [LoginRedirect] },
 ];
 
 @NgModule({
